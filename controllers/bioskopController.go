8e78f87da7a0b150bcfa6c6c23f8d55c2828423a
reyhansep3 <reyhanseptri@gmail.com>
2025-07-17 23:07:57 +0700

    push tugas hari 14

--- controllers/bioskopController.go
+++ controllers/bioskopController.go
@@ -2,7 +2,9 @@ package controller
 
 import (
 	"database/sql"
+	"fmt"
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
@@ -46,5 +48,130 @@ func CreateBioskop(db *sql.DB) gin.HandlerFunc {
 		ctx.JSON(http.StatusCreated, gin.H{
 			"bioskop": newBioskop,
 		})
+	}
+}
+
+// jawaban tugas hari 14
+func GetBioskop(db *sql.DB) gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		var getBioskop []Bioskop
+
+		sqlStatement := `SELECT * from bioskop`
+
+		rows, err := db.Query(sqlStatement)
+		if err != nil {
+			panic(err)
+		}
+
+		defer rows.Close()
+
+		for rows.Next() {
+			var bioskop = Bioskop{}
+
+			err = rows.Scan(&bioskop.ID, &bioskop.Nama, &bioskop.Lokasi, &bioskop.Rating)
+
+			if err != nil {
+				panic(err)
+			}
+			getBioskop = append(getBioskop, bioskop)
+		}
+
+		fmt.Println("data bioskop", getBioskop)
+		ctx.JSON(http.StatusOK, gin.H{
+			"bioskop": getBioskop,
+		})
+	}
+}
+
+func GetBioskopByID(db *sql.DB) gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		idParams := ctx.Param("id")
+		var bioskop Bioskop
+
+		query := `SELECT * FROM bioskop WHERE id = $1`
+		id, err := strconv.Atoi(idParams)
+		if err != nil {
+			panic(err)
+		}
+
+		err = db.QueryRow(query, id).Scan(&bioskop.ID, &bioskop.Nama, &bioskop.Lokasi, &bioskop.Rating)
+		if err != nil {
+			if err == sql.ErrNoRows {
+				ctx.JSON(http.StatusNotFound, gin.H{"error": "Bioskop tidak ditemukan"})
+				return
+			}
+			ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+
+		ctx.JSON(http.StatusOK, gin.H{"bioskop": bioskop})
+	}
+}
+
+func UpdateBioskop(db *sql.DB) gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		idParams := ctx.Param("id")
+		id, err := strconv.Atoi(idParams)
+		if err != nil {
+			panic(err)
+		}
+		var bioskop Bioskop
+		if err := ctx.ShouldBindJSON(&bioskop); err != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{"error": "Invalid input"})
+			return
+		}
+
+		sqlStatement := `
+		UPDATE bioskop
+		SET nama = $2, lokasi = $3, rating = $4
+		WHERE id = $1;
+		`
+		hasil, err := db.Exec(sqlStatement, id, bioskop.Nama, bioskop.Lokasi, bioskop.Rating)
+		if err != nil {
+			panic(err)
+		}
+
+		rowsAffected, err := hasil.RowsAffected()
+		if err != nil {
+			panic(err)
+		}
+
+		if rowsAffected == 0 {
+			ctx.JSON(http.StatusNotFound, gin.H{"Error": "data not found"})
+		}
+
+		ctx.JSON(http.StatusOK, gin.H{"bioskop": bioskop})
+	}
+}
+
+func DeleteBioskop(db *sql.DB) gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		paramId := ctx.Param("id")
+		var bioskop Bioskop
+		if err := ctx.ShouldBindJSON(&bioskop); err != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{"error": "Invalid input"})
+			return
+		}
+		id, err := strconv.Atoi(paramId)
+		if err != nil {
+			panic(err)
+		}
+		sqlStatement := `DELETE FROM bioskop WHERE id = $1`
+		hasil, err := db.Exec(sqlStatement, id)
+		if err != nil {
+			panic(err)
+		}
+
+		rowsAffected, err := hasil.RowsAffected()
+		if err != nil {
+			panic(err)
+		}
+
+		if rowsAffected == 0 {
+			ctx.JSON(http.StatusNotFound, gin.H{
+				"error": "Data not found",
+			})
+		}
+		ctx.JSON(http.StatusOK, gin.H{"data": "data has deleted"})
 	}
 }
--- routers/bioskopRouter.go
+++ routers/bioskopRouter.go
@@ -11,5 +11,12 @@ func StartServer(db *sql.DB) *gin.Engine {
 	router := gin.Default()
 
 	router.POST("/bioskop", controller.CreateBioskop(db))
+
+	// jawaban tugas hari 14
+	router.GET("/bioskop", controller.GetBioskop(db))
+	router.GET("/bioskop/:id", controller.GetBioskopByID(db))
+	router.PUT("/bioskop/:id", controller.UpdateBioskop(db))
+	router.DELETE("/bioskop/:id", controller.DeleteBioskop(db))
+
 	return router
 }
